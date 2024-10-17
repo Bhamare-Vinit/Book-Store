174def439d495aa1e50d5fbbba6cbde8e381ec2b
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-10-17 14:01:49 +0530

    [vinit] add:user login and register, user model

---
+++ user/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import authenticate
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from .models import User
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(request, email=serializer.validated_data['email'], password=serializer.validated_data['password'])
+            if user:
+                return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
